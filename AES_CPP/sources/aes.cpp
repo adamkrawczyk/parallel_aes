4ad3fb636f63cb3b3b47056dcbb6f06fd61459a2
Eryk Wawrzyn <eryk.wawrzyn.stud@pw.edu.pl>
2021-11-18 12:26:09 +0100

    implementation of KeyExpansion, AddRoundKey, ArrayTransformOneDim, ArrayTransformTwoDim, Cipher function

--- AES_CPP/sources/aes.cpp
+++ AES_CPP/sources/aes.cpp
@@ -2,9 +2,60 @@
 
 AES::AES(int Nk_tmp, int Nr_tmp, int Nb_tmp)
 {
-    this->Nk = Nk_tmp;
-    this->Nr = Nr_tmp;
-    this->Nb = Nb_tmp;
+    Nk = Nk_tmp;
+    Nr = Nr_tmp;
+    Nb = Nb_tmp;
+}
+
+void AES::KeyExpansion(w_type *key, w_type *w)
+{
+    w_type *temp = (w_type*)malloc(Nb * sizeof(w_type*));
+
+    int i = 0;
+
+    while(i < Nb * Nk)
+    {
+        w[i] = key[i];
+        i++;
+    }
+
+    while(i < Nb * (Nr+1))
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            temp[j] = w[(i-1) * 4 + j];
+        }
+
+        if(i % Nk == 0)
+        {
+            state_type tmp = temp[0];
+            for(int j = 0; j < Nb - 1; j++)
+            {
+                temp[j] = temp[j+1];
+            }
+
+            temp[Nb - 1] = tmp;
+
+            for(int j = 0; j < Nb; j++)
+            {
+                temp[j] = sbox[temp[j]] ^ Rcon[i/Nk];//do poprawy
+            }
+
+        }
+        else if(Nk > 6 && i % Nk == 4)
+        {
+            for(int j = 0; j < Nb; j++)
+            {
+                temp[j] = sbox[temp[j]];
+            }
+        }
+        for(int j = 0; j < Nb; j++)
+        {
+            w[j + i*Nb] = w[(i - Nk)*Nb] ^ temp[j];
+        }
+
+        i++;
+    }
 }
 
 void AES::SubBytes(state_type **state)
@@ -13,12 +64,11 @@ void AES::SubBytes(state_type **state)
     {
         for(int j = 0; j < Nb; j++)
         {
-            state[i][j] = rsbox[state[i][j]];
+            state[i][j] = sbox[state[i][j]];
         }
     }
 
 }
-
 
 void AES::ShiftRows(state_type **state)
 {
@@ -34,4 +84,72 @@ void AES::ShiftRows(state_type **state)
             state[numberOfShifts][Nb - 1] = tmp;
         }
     }
+}
+
+void AES::AddRoundKey(state_type  **state, w_type *w)
+{
+    for(int j = 0; j < Nb; j++)
+    {
+        for(int i = 0; i < Nb; i++)
+        {
+            state[j][i] = state[j][i] ^ w[j + Nb * i]; //"w" pseudo conversion to a 2-dimensional array
+        }
+
+    }
+}
+
+void AES::MixColumns(state_type **state)
+{
+
+}
+
+void AES::ArrayTransformOneDim(state_type *in, state_type **state)
+{
+    for (int i = 0; i < Nb; i++)
+    {
+        for (int j = 0; j < Nb; j++)
+        {
+            state[i][j] = in[i + Nb * j];
+        }
+    }
+}
+
+void AES::ArrayTransformTwoDim(state_type *out, state_type **state)
+{
+    for (int i = 0; i < Nb; i++)
+    {
+        for (int j = 0; j < Nb; j++)
+        {
+            out[i + Nb * j] = state[i][j];
+        }
+    }
+}
+
+void AES::Cipher(state_type *in, state_type *out, w_type *w)
+{
+    state_type **state = (state_type**)malloc(Nb * sizeof(state_type*));
+
+    for (int i = 0; i < Nb; i++)
+    {
+        state[i] = (state_type*)malloc(Nb * sizeof(state_type));
+    }
+
+    AES::ArrayTransformOneDim(in, state);
+
+    AES::AddRoundKey(state, w); 
+
+    for(int round = 1; round <= Nr - 1; round++)
+    {
+        AES::SubBytes(state);
+        AES::ShiftRows(state);
+        AES::MixColumns(state);
+        AES::AddRoundKey(state, (w + round * Nb * Nb)); //I don't understand what values mean in pseudocode, I used the description from https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+    }
+
+    AES::SubBytes(state);
+    AES::ShiftRows(state);
+    AES::AddRoundKey(state, (w + Nr * Nb * Nb));
+
+    ArrayTransformTwoDim(out, state);
+
 }
--- AES_CPP/sources/main.cpp
+++ AES_CPP/sources/main.cpp
@@ -4,39 +4,4 @@ int main()
 {
     AES tmp(8, 14, 4); 
 
-    /* a very simplified ShiftRows test
-    int **tab = (int**)malloc(4 * sizeof(int*));
-    for (int i = 0; i < 4; i++)
-        tab[i] = (int*)malloc(4 * sizeof(int));
-
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 0; j < 4; j++)
-        {
-            tab[i][j] =  j;
-        }
-    }
-
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 0; j < 4; j++)
-        {
-            std::cout << tab[i][j] << " ";
-        }
-
-        std::cout << "\n";
-    }
-
-    std::cout << "\n";
-    tmp.ShiftRows(tab);
-
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 0; j < 4; j++)
-        {
-            std::cout << tab[i][j] << " ";
-        }
-
-        std::cout << "\n";
-    }*/
 }
