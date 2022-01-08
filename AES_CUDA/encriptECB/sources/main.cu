0211fd2fafe154d7f3e73846550c09b9d8c71376
Marcin Skrzypkowski <mj.skrzypkowski@gmail.com>
2022-01-08 18:59:21 +0100

    [CUDA]  compile encript program without warnings, wrong result though

--- AES_CUDA/encriptECB/sources/main.cu
+++ AES_CUDA/encriptECB/sources/main.cu
@@ -1,12 +1,16 @@
 #include "AES.h"
 
+__device__
 void keyExpansion(w_type key[KEY_LEN], w_type w[KEY_ROUND]) {
 	w_type temp[Nb];
 
 	int i = 0;
 
-	memcpy(w, key, Nb * Nk * sizeof(w_type));
-	
+	for(i = 0; i < Nb * Nk; i++)
+	{
+		w = key;
+	}
+
 	i = Nk;
 
 	while (i < Nb * (Nr + 1)) {
@@ -41,6 +45,7 @@ void keyExpansion(w_type key[KEY_LEN], w_type w[KEY_ROUND]) {
 	}
 }
 
+__device__
 void subBytes(state_type state[Nb][Nb]) {
 	for (int i = 0; i < Nb; i++) {
 		for (int j = 0; j < Nb; j++) {
@@ -49,6 +54,7 @@ void subBytes(state_type state[Nb][Nb]) {
 	}
 }
 
+__device__
 void shiftRows(state_type state[Nb][Nb]) {
 	for (int numberOfShifts = 0; numberOfShifts < Nb; numberOfShifts++) {
 		for (int j = 0; j < numberOfShifts; j++) {
@@ -61,6 +67,7 @@ void shiftRows(state_type state[Nb][Nb]) {
 	}
 }
 
+__device__
 void addRoundKey(state_type state[Nb][Nb], w_type w[KEY_ROUND]) {
 	for (int j = 0; j < Nb; j++) {
 		for (int i = 0; i < Nb; i++) {
@@ -70,12 +77,13 @@ void addRoundKey(state_type state[Nb][Nb], w_type w[KEY_ROUND]) {
 	}
 }
 
+__device__
 void mixColumns(state_type state[Nb][Nb]) {
 
 	state_type r[Nb];
 	state_type a[Nb];
 	state_type b[Nb];
-	state_type h;
+	// state_type h;
 
 	for (int i = 0; i < Nb; i++) {
 		for(int j = 0; j < Nb; j++)
@@ -102,6 +110,7 @@ void mixColumns(state_type state[Nb][Nb]) {
 	}
 }
 
+__device__
 void arrayTransformOneDim(state_type in[IN_LEN], state_type state[Nb][Nb]) {
 	for (int i = 0; i < Nb; i++) {
 		for (int j = 0; j < Nb; j++) {
@@ -110,6 +119,7 @@ void arrayTransformOneDim(state_type in[IN_LEN], state_type state[Nb][Nb]) {
 	}
 }
 
+__device__
 void arrayTransformTwoDim(state_type out[OUT_LEN], state_type state[Nb][Nb]) {
 	for (int i = 0; i < Nb; i++) {
 		for (int j = 0; j < Nb; j++) {
@@ -118,6 +128,7 @@ void arrayTransformTwoDim(state_type out[OUT_LEN], state_type state[Nb][Nb]) {
 	}
 }
 
+__device__
 void cipher(state_type in[IN_LEN], state_type out[OUT_LEN],
 		w_type w[KEY_ROUND]) {
 	state_type state[Nb][Nb];
@@ -141,12 +152,65 @@ void cipher(state_type in[IN_LEN], state_type out[OUT_LEN],
 
 }
 
-// __device__
-// void encriptECB(state_type in[IN_LEN], state_type out[OUT_LEN],
-// 		w_type key[KEY_LEN]) {
-// // #pragma HLS PIPELINE
-// 	w_type w[KEY_ROUND];
-// 	keyExpansion(key, w);
-// 	cipher(in, out, w);
-// }
+__device__
+void encriptECB(state_type in[IN_LEN], state_type out[OUT_LEN],
+		w_type key[KEY_LEN]) {
+// #pragma HLS PIPELINE
+	w_type w[KEY_ROUND];
+	keyExpansion(key, w);
+	cipher(in, out, w);
+}
 
+__global__
+static void ecb_encrypt_kernel(state_type* in, state_type* out, w_type* key)
+{
+    printf("hello from thread %d\n", threadIdx.x);
+    encriptECB(in, out, key);
+}
+
+int main() {
+
+	int key[] = { 0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b,
+			0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07,
+			0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf,
+			0xf4 };
+	int right[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06,
+			0x4b, 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8 };
+
+	int plain[] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9,
+			0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
+
+	state_type out[OUT_LEN];
+
+    int *key_gpu, *plain_gpu, *out_gpu;
+
+    checkCudaErrors(cudaSetDevice(0));
+
+    checkCudaErrors(cudaMalloc(&key_gpu, sizeof(int)*32));
+    checkCudaErrors(cudaMalloc(&out_gpu, sizeof(int)*16));
+    checkCudaErrors(cudaMalloc(&plain_gpu, sizeof(int)*16));
+
+    checkCudaErrors(cudaMemcpy(key_gpu, key, sizeof(int)*32, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(plain_gpu, plain, sizeof(int)*16, cudaMemcpyHostToDevice));
+    
+    ecb_encrypt_kernel<<<1, 1>>>(plain_gpu, out_gpu, key_gpu);
+    checkCudaErrors(cudaGetLastError());
+    
+    checkCudaErrors(cudaMemcpy(out, out_gpu, sizeof(int)*16, cudaMemcpyDeviceToHost));
+	for (int i = 0; i < OUT_LEN; i++) {
+		printf("%02x ", (unsigned char) out[i]);
+	}
+
+	if (0 == memcmp((char*) out, (char*) right, 16)) {
+		printf("SUCCESS!\n");
+		return (0);
+	} else {
+		printf("FAILURE!\n");
+		return (1);
+	}
+
+    checkCudaErrors(cudaFree(key_gpu));
+    checkCudaErrors(cudaFree(plain_gpu));
+
+    checkCudaErrors(cudaDeviceReset());
+}
