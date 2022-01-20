4d1c550bd59b748f1f8884b012374e74789048c7
Marcin Skrzypkowski <mj.skrzypkowski@gmail.com>
2022-01-20 01:29:15 +0100

    [CUDA] properly encrypt and decrypt data with length multiple of 16

--- AES_CUDA/encriptECB/sources/main.cu
+++ AES_CUDA/encriptECB/sources/main.cu
@@ -10,8 +10,8 @@ static void ecb_encrypt_kernel(state_type* in, state_type* out, w_type* key)
 
 int main() {
 	std::ifstream in_file;
-	in_file.open("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/in_str.txt", std::ios::binary);
-    std::size_t file_size = std::experimental::filesystem::file_size("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/in_str.txt");
+	in_file.open("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/plain.txt", std::ios::binary);
+    std::size_t file_size = std::experimental::filesystem::file_size("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/plain.txt");
     int padding = file_size % 16;
     char plain[file_size+padding];
 
@@ -24,13 +24,19 @@ int main() {
     in_file.read(plain, file_size);
 	in_file.close();
 
+    // fill padding with zeros
+    for(int i=0;i<padding;i++)
+    {
+        plain[file_size+i] = 0x00;
+    }
+
 	uint8_t key[] = { 0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b,
         0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07,
         0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf,
         0xf4 };
     // uint8_t right_base[] = { 0xf3, 0xee, 0xd1, 0xbd, 0xb5, 0xd2, 0xa0, 0x3c, 0x06,
     //     0x4b, 0x5a, 0x7e, 0x3d, 0xb1, 0x81, 0xf8};
-    // uint8_t plain_base[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9,
+    // uint8_t plain[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9,
     //     0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a};
     // uint8_t right[file_size+padding]; 
 
@@ -51,7 +57,10 @@ int main() {
     cudaEvent_t start, stop; // pomiar czasu wykonania j?dra
 
     for(int i=0; i< file_size;i++)
-        std::cout<<(int)plain[i]<<" ";
+    {
+        std::cout<<(uint8_t)plain[i]<<" ";
+
+    }
     std::cout<<"\n";
 
     checkCudaErrors(cudaSetDevice(0));
@@ -66,17 +75,20 @@ int main() {
     checkCudaErrors(cudaMemcpy(key_gpu, key, sizeof(uint8_t)*32, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(plain_gpu, plain, sizeof(uint8_t)*(file_size+padding), cudaMemcpyHostToDevice));
     
-    ecb_encrypt_kernel<<<1, 1/*file_size/16*/>>>(plain_gpu, out_gpu, key_gpu);
+    ecb_encrypt_kernel<<<1, file_size/16>>>(plain_gpu, out_gpu, key_gpu);
     checkCudaErrors(cudaGetLastError());
     
     checkCudaErrors(cudaMemcpy(out, out_gpu, sizeof(uint8_t)*(file_size+padding), cudaMemcpyDeviceToHost));
 
-    for(int i=0; i< file_size;i++)
+    for(int i=0; i< file_size+padding;i++)
+    {
         std::cout<<(int)out[i]<<" ";
+        // itoa(out[i], plain[i], 10);
+    }
     std::cout<<"\n";
 
 	std::ofstream out_file;
-    out_file.open("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/out_str.txt", std::ios::binary);
+    out_file.open("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/encrypted.txt", std::ios::binary);
     out_file.write((char *)out, file_size);
     out_file.close();
 
