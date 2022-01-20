3eb0c489e624c7062dcbb695123782c7fa0593d4
Marcin Skrzypkowski <mj.skrzypkowski@gmail.com>
2022-01-21 00:03:01 +0100

    [CUDA] optimizations - keyExpansion out of gpu, unrolled for loops

--- AES_CUDA/encriptECB/sources/main.cu
+++ AES_CUDA/encriptECB/sources/main.cu
@@ -35,48 +35,55 @@ int main() {
         0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf,
         0xf4 };
 
+
 	state_type out[file_size+padding];
 
     uint8_t *key_gpu, *plain_gpu, *out_gpu;
-    float elapsedTime;
-    cudaEvent_t start, stop; // pomiar czasu wykonania j?dra
+    float elapsedTime, kernelTime;
+    cudaEvent_t start, kernel_start, kernel_stop, stop; // pomiar czasu wykonania j?dra
 
-    for(int i=0; i< file_size+padding;i++)
-    {
-        std::cout<<(int)plain[i]<<" ";
+    // for(int i=0; i< file_size+padding;i++)
+    // {
+    //     std::cout<<(int)plain[i]<<" ";
 
-    }
-    std::cout<<"\n";
+    // }
+    // std::cout<<"\n";
 
     checkCudaErrors(cudaSetDevice(0));
 
     checkCudaErrors(cudaEventCreate(&start));
+    checkCudaErrors(cudaEventCreate(&kernel_start));
+    checkCudaErrors(cudaEventCreate(&kernel_stop));
     checkCudaErrors(cudaEventCreate(&stop));
     checkCudaErrors(cudaEventRecord(start, 0));
-    checkCudaErrors(cudaMalloc(&key_gpu, sizeof(uint8_t)*32));
+    w_type w[KEY_ROUND];
+    keyExpansion(key, w);
+
+    checkCudaErrors(cudaMalloc(&key_gpu, sizeof(uint8_t)*KEY_ROUND));
     checkCudaErrors(cudaMalloc(&out_gpu, sizeof(uint8_t)*(file_size+padding)));
     checkCudaErrors(cudaMalloc(&plain_gpu, sizeof(uint8_t)*(file_size+padding)));
 
-    checkCudaErrors(cudaMemcpy(key_gpu, key, sizeof(uint8_t)*32, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(key_gpu, w, sizeof(uint8_t)*KEY_ROUND, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(plain_gpu, plain, sizeof(uint8_t)*(file_size+padding), cudaMemcpyHostToDevice));
     
+    checkCudaErrors(cudaEventRecord(kernel_start, 0));
     ecb_encrypt_kernel<<<1, (file_size+padding)/16>>>(plain_gpu, out_gpu, key_gpu);
     checkCudaErrors(cudaGetLastError());
-    
+    checkCudaErrors(cudaEventRecord(kernel_stop, 0));
+
     checkCudaErrors(cudaMemcpy(out, out_gpu, sizeof(uint8_t)*(file_size+padding), cudaMemcpyDeviceToHost));
 
     checkCudaErrors(cudaEventRecord(stop, 0));
 
-    for(int i=0; i< file_size+padding;i++)
-    {
-        std::cout<<(int)out[i]<<" ";
-        // itoa(out[i], plain[i], 10);
-    }
+    // for(int i=0; i< file_size+padding;i++)
+    // {
+    //     std::cout<<(int)out[i]<<" ";
+    //     // itoa(out[i], plain[i], 10);
+    // }
     
 	std::ofstream out_file;
     out_file.open("/home/silver/My-projects/CUDA/samples/0_Simple/project/AES_CUDA/encriptECB/data/encrypted.txt", std::ios::binary);
-    // write padding size
-    sprintf(padding_char, "%d", padding);
+
     std::cout<<"\n"<<"padding is "<<padding_char[0]<<padding_char[1]<<", full file size is "<<file_size+padding<<"\n";
     // out_file.write(padding_char, 2);
     out_file.write((char *)out, file_size+padding);
@@ -84,10 +91,12 @@ int main() {
 
     checkCudaErrors(cudaEventSynchronize(stop));
     checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
+    checkCudaErrors(cudaEventElapsedTime(&kernelTime, kernel_start, kernel_stop));
     checkCudaErrors(cudaEventDestroy(start));
     checkCudaErrors(cudaEventDestroy(stop));
 
     printf("GPU time = %.3f ms\n", elapsedTime);
+    printf("GPU kernel time = %.3f ms\n", kernelTime);
 
     checkCudaErrors(cudaFree(key_gpu));
     checkCudaErrors(cudaFree(plain_gpu));
